c38b645b70913e2afc1e506eeeabdbab4b6fa870
nilq <i.am@nilq.dk>
2017-02-12 20:26:55 +0100

    Add syntax structure

---
+++ src/Syntax.hs
@@ -0,0 +1,18 @@
+module Syntax where
+
+type Name =
+  String
+
+data Expr =
+    Float Double
+  | Var String
+  | Call Name [Expr]
+  | Function Name [Name] Expr
+  | Extern Name [Name]
+  | BinaryOp Name Expr Expr
+  | UnaryOp Name Expr
+  | If Expr Expr Expr
+  | For Name Expr Expr Expr Expr
+  | Declare Name Expr Expr
+
+  deriving (Eq, Ord, Show)
