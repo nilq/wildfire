8ff8694da3eb06d77f1cedbc31fea8c14e38b218
nilq <i.am@nilq.dk>
2017-02-12 20:24:28 +0100

    Add lexer structure

---
+++ src/Lexer.hs
@@ -0,0 +1,59 @@
+module Lexer where
+
+import Text.Parsec.String (Parser)
+import Text.Parsec.Language (emptyDef)
+
+import qualified Text.Parsec.Token as Tok
+
+lexer :: Tok.TokenParser ()
+lexer =
+  Tok.makeTokenParser style
+    where
+      ops =
+        [ "+"
+        , "-"
+        , "*"
+        , "/"
+        , ","
+        , "<"
+        , ">"
+        ]
+
+      names =
+        [ "func"
+        , "summon"
+        , "if"
+        , "then"
+        , "else"
+        ]
+
+      style =
+        emptyDef
+          { Tok.commentLine     = "#"
+          , Tok.reservedOpNames = ops
+          , Tok.reservedNames   = names
+          }
+
+integer =
+  Tok.integer lexer
+
+float =
+  Tok.float lexer
+
+parens =
+  Tok.parens lexer
+
+commaSep =
+  Tok.commaSep lexer
+
+identifier =
+  Tok.identifier lexer
+
+reserved =
+  Tok.reserved lexer
+
+reservedOp =
+  Tok.reservedOp lexer
+
+whitespace =
+  Tok.whiteSpace lexer
